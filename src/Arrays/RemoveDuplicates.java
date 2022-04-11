b8db7352f01d43452f5319e6ed0a6f73c1b0caec
Ram11Coder <rambeliver11@gmail.com>
2022-04-11 22:24:39 +0530

    Added RemoveDuplcates

---
+++ src/Arrays/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class RemoveDuplicates {
+	public static void main(String[] args) {
+		System.out.println(removeDuplicates(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }));
+	}
+
+	public static int removeDuplicates(int[] nums) {
+		int j = 1;
+		for (int i = 0; i < nums.length - 1; i++)
+			if (nums[i] != nums[i + 1])
+				nums[j++] = nums[i + 1];
+
+	//	System.out.println(Arrays.toString(nums));
+		return j;
+	}
+
+}
