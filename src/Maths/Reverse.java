9125169e797c311d8cdfbfaab27f1d3edf6db198
Ram11Coder <rambeliver11@gmail.com>
2022-04-29 21:46:04 +0530

    Updated reverse program

--- src/Maths/Reverse.java
+++ src/Maths/Reverse.java
@@ -1,23 +1,27 @@
 package Maths;
 
-public class Reverse {
-	public static void main(String[] args) {
-		
-		System.out.println(reverse(1234));
-		System.out.println(reverse(-123));
-		System.out.println(reverse(-8));
-		System.out.println(reverse(1231342234));
-		System.out.println(reverse(999999991));
-		System.out.println(reverse(-1231342238));
-		System.out.println(reverse(124000000));
-		System.out.println(reverse(555555));
-		System.out.println(reverse(00000000));
-		System.out.println(reverse(102030405));
-		System.out.println(reverse(2147483647));//Integer max
-		System.out.println(reverse(-2147483647));
-		System.out.println(reverse(-2147483648));//Integer min
-		System.out.println(reverse(-1463847412));
+import java.security.NoSuchAlgorithmException;
 
+public class Reverse {
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		
+		int maxKeySize = javax.crypto.Cipher.getMaxAllowedKeyLength("AES");
+		
+		System.out.println(maxKeySize);
+		
+		/*
+		 * System.out.println(reverse(1234)); System.out.println(reverse(-123));
+		 * System.out.println(reverse(-8)); System.out.println(reverse(1231342234));
+		 * System.out.println(reverse(999999991));
+		 * System.out.println(reverse(-1231342238));
+		 * System.out.println(reverse(124000000)); System.out.println(reverse(555555));
+		 * System.out.println(reverse(00000000));
+		 * System.out.println(reverse(102030405));
+		 * System.out.println(reverse(2147483647));//Integer max
+		 * System.out.println(reverse(-2147483647));
+		 * System.out.println(reverse(-2147483648));//Integer min
+		 * System.out.println(reverse(-1463847412));
+		 */
 	}
 
 	public static int reverse(int x) {
