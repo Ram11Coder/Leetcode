5debfde84f4aa5a96de694ea3aa9e7aa49ab4d34
Ram11Coder <rambeliver11@gmail.com>
2022-10-28 21:32:38 +0530

    Formatted the code

--- src/HashTable/HappyNumber.java
+++ src/HashTable/HappyNumber.java
@@ -18,13 +18,14 @@ public class HappyNumber {
 	 * A happy number is a number defined by the following process:
 	 * 
 	 * Starting with any positive integer, replace the number by the sum of the
-	 * squares of its digits. Repeat the process until the number equals 1 (where it
-	 * will stay), or it loops endlessly in a cycle which does not include 1. Those
-	 * numbers for which this process ends in 1 are happy.
+	 * squares of its digits.
+	 * 
+	 * Repeat the process until the number equals 1 (where it will stay), or it
+	 * loops endlessly in a cycle which does not include 1.
+	 * 
+	 * Those numbers for which this process ends in 1 are happy.
 	 * 
 	 * If same number return more than once then that is not a happy number
-	 * 
-	 * i
 	 * 
 	 * @param n
 	 * @return
