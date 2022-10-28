e894dd3ed9d32d32c3f5d478583c72fd575c6f14
Ram11Coder <rambeliver11@gmail.com>
2022-10-28 10:00:46 +0530

    Added map solution

--- src/Arrays/FirstUniqueCharacterInAString.java
+++ src/Arrays/FirstUniqueCharacterInAString.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.HashMap;
+
 public class FirstUniqueCharacterInAString {
 	public static void main(String[] args) {
 		System.out.println(firstUniqChar("leetcode"));
@@ -14,6 +16,23 @@ public class FirstUniqueCharacterInAString {
 			if (freq[s.charAt(i) - 'a'] == 1)
 				return i;
 
+		return -1;
+	}
+
+	public int firstUniqChar_Map(String s) {
+		HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+		int n = s.length();
+		// build hash map : character and how often it appears
+		for (int i = 0; i < n; i++) {
+			char c = s.charAt(i);
+			count.put(c, count.getOrDefault(c, 0) + 1);
+		}
+
+		// find the index
+		for (int i = 0; i < n; i++) {
+			if (count.get(s.charAt(i)) == 1)
+				return i;
+		}
 		return -1;
 	}
 }
